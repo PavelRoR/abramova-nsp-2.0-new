409c9879d97185fc377dba7ced7c6718c922b990
pavelp21 <pavelvp2182@yandex.ru>
2020-07-31 15:54:59 +0300

    Main page is stylezed

--- _app/_js/scripts.js
+++ _app/_js/scripts.js
@@ -1,7 +1,31 @@
 //@prepros-prepend jquery-2.1.1.min.js
 //@prepros-prepend bootstrap.min.js
+/*@prepros-prepend jquery.event.frame.js*/
+/*@prepros-prepend jquery.parallax.min.js*/
+//@prepros-prepend owl.carousel.min.js
+//@prepros-prepend popup.js
 
 $(document).ready(function () {
+    $(function () {
+        var img1 = $('.main-img-1');
+        window.addEventListener('mousemove', function (e) {
+            let x = e.clientX / window.innerWidth;
+            let y = e.clientY / window.innerHeight;
+            img1.css('transform', 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)');
+        });
+        var img2 = $('.main-img-2');
+        window.addEventListener('mousemove', function (e) {
+            let x = e.clientX / window.innerWidth;
+            let y = e.clientY / window.innerHeight;
+            img2.css('transform', 'translate(-' + x * 40 + 'px, -' + y * 40 + 'px)');
+        });
+        var img3 = $('.main-img-3');
+        window.addEventListener('mousemove', function (e) {
+            let x = e.clientX / window.innerWidth;
+            let y = e.clientY / window.innerHeight;
+            img3.css('transform', 'translate(-' + x * 30 + 'px, -' + y * 30 + 'px)');
+        });
+    });
     $(function () {
         var check = $('.check', this),
             email = $('.input-mail', this),
@@ -87,41 +111,50 @@ $(document).ready(function () {
             message.slideUp(500);
         });
     });
-    $('.button-up, [data-fancybox]').fancybox();
-    $(function () {
-        $("[data-fancybox]").fancybox({
-            buttons: [
-                'slideShow',
-                'share',
-                'zoom',
-                'fullScreen',
-                // 'download',
-                'close'
-            ],
-            speed: 330,
-            loop: true,
-            opacity: "auto",
-            // autoScale: true,
-            mouseWheel: true,
-            transitionEffect: 'slide'
-        });
+    $('.button-up').magnificPopup({
+        type: 'inline',
+        midClick: true, // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+        mainClass: 'mfp-fade'
     });
-    $(".video-wrapper-revs img").click(function () {
+    // var w_height = $(this).height();
+    // var slide_width = $('.name-title-container').width();
+    // var slide_left = slide_width / w_height;
+    $(window).scroll(function (e) {
+        var aa = $(this).scrollTop() * -1;
+        $('.name-title-img, .divider-img').animate({ left: aa + 'px' }, 10);
+    });
+
+    /* Видео */
+    $(".video-wrapper-rev img").click(function () {
         var a = $(this).parent().attr("data-youtube");
-        $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1&mute=1"  allowfullscreen></iframe>')
+        $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1&modestbranding=1&mute=1&showinfo=0&rel=0" allowfullscreen></iframe>')
     });
     $(function () {
-        $('#video-revs').on('slide.bs.carousel', function () {
-            $('.video-wrapper-revs iframe').each(function () {
+        $('#video-revs .owl-prev, #video-revs .owl-next').click(function () {
+            $('.video-wrapper-rev iframe').each(function () {
                 var l = $(this).parent().attr('data-img');
                 $(this).parent().html('<img src="' + l + '" alt="Видео отзыв">');
             });
-            $(".video-wrapper-revs img").click(function () {
+            $(".video-wrapper-rev img").click(function () {
                 var a = $(this).parent().attr("data-youtube");
-                $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1"  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>');
+                $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?modestbrandig=1&mute=1&showinfo=0&rel=0&autoplay=1" allowfullscreen></iframe>');
             });
         });
+    })
+    $('#video-revs, #text-revs').owlCarousel({
+        items: 4,
+        loop: true,
+        margin: 10,
+        nav: true,
+        navText: ["←", "→"],
+        dots: false
     });
-
-    /*Конец документа*/
+    $('.text-rev-link').magnificPopup({
+        type: 'image',
+        mainClass: 'mfp-fade',
+        gallery: {
+            enabled: true
+        }
+    });
 });
+    /*Конец документа*/
